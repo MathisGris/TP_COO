e3605440dd83ff9969d2f11d376c99c3a8e88270
MathisGris <mathis.gris1@gmail.com>
2025-10-01 10:41:35 +0200

    Ajout du constructeur prenant json data en paramètre et le constructeur prennant id en paramètre

--- tp_coo/boissons/low_level/low_level.cpp
+++ tp_coo/boissons/low_level/low_level.cpp
@@ -7,13 +7,53 @@ class Localisation {
     public:
         Localisation() {}
         Localisation(const std::string& nom, const std::string& taxes, float prix_m2) : nom{nom}, taxes{taxes}, prix_m2{prix_m2} {}
+        Localisation(nlohmann::json data) : nom{data["nom"]}, taxes{data["taxes"]}, prix_m2{data["prix_m2"]} {}
+        Localisation(int id) {
+            std::string str_id = std::to_string(id);
+            cpr::Response patate = cpr::Get(cpr::Url{"http://localhost:8000/localisation/"+str_id});
+            nlohmann::json data = nlohmann::json::parse(patate.text);
+            nom = data["nom"];
+            taxes = data["taxes"];
+            prix_m2 = data["prix_m2"];    
+        }
 
         std::string nom;
         float prix_m2;
         std::string taxes;
         
-        auto affichage() {std::cout << "Nom :" << nom << "\nTaxes :" << taxes << "\nprix :" << prix_m2 << "euros au m2";}
+        auto affichage() {std::cout << "\nNom :" << nom << "\nTaxes :" << taxes << "\nprix :" << prix_m2 << "euros au m2";}
 };
+
+class Local {
+    public:
+        Local() {}
+        Local(const std::string& nom, const std::string& taxes, float prix_m2) : nom{nom}, taxes{taxes}, prix_m2{prix_m2} {}
+        Local(nlohmann::json data) : nom{data["nom"]}, taxes{data["taxes"]}, prix_m2{data["prix_m2"]} {}
+        Local(int id) {
+            std::string str_id = std::to_string(id);
+            cpr::Response patate = cpr::Get(cpr::Url{"http://localhost:8000/localisation/"+str_id});
+            nlohmann::json data = nlohmann::json::parse(patate.text);
+            nom = data["nom"];
+            localisation = data["localisation"];
+            surface = data["surface"];    
+        }
+
+        std::string nom;
+        std::unique_ptr<> localisation
+        float surface;
+        
+        auto affichage() {std::cout << "\nNom :" << nom << "\nLocalisation :" << localisation << "\nsurface :" << surface << "m2";}
+};
+
+
+
+
+
+
+
+
+
+
 
 auto main() -> int {
     auto localisation = Localisation{"Courchevel", "20%", 5000};
@@ -24,13 +64,10 @@ auto main() -> int {
 
     nlohmann::json data = nlohmann::json::parse(patate.text);
 
-    std::string nom = data["nom"];
-    std::string taxes_str = data["taxes"];
-    double prix_m2 = data["prix_m2"];
+    auto lieu = Localisation{1};
+    lieu.affichage();
 
-    std::cout << "Nom     : " << nom << "\n";
-    std::cout << "Taxes   : " << taxes_str << "\n";
-    std::cout << "Prix m2 : " << prix_m2 << "\n";
+
 
     return 0;
 }
